02b0e469b62e0a103471c3b6a598bb6f2e013261
Javier Olivieri <cosoligroup@gmail.com>
2022-02-14 16:34:51 -0300

    Clases

---
+++ ejercicio1.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(name, lastName, pet) {
+        this.name = name;
+        this.lastName = lastName;
+        this.books = [];
+        this.pets = [pet];
+    }
+    getFullName() {
+        return `${this.name} ${this.lastName}`;
+    }
+    addPet(pet) {
+        this.pets.push(pet);
+    }
+    countPets() {
+        return this.pets.length;
+    }
+    addBook(name, autor) {
+        const book = {
+            name: name,
+            autor: autor,
+        };
+        this.books.push(book);
+    }
+    getBookNames() {
+        return this.books.map(({ name }) => name);
+    }
+}
+const user = new User("Javier", "Olivieri", "gato");
+console.log(user.getFullName());
+user.addPet("perro");
+console.log(user.countPets());
+user.addBook("Padre Rico Padre Pobre", "Robert Kiyosaqui");
+user.addBook("La vida invisible 2", "Addie Larue");
+console.log(user.getBookNames());
